dcf9b6795b54c7993ef1bf19e08cb9630db098e4
Tom Arrow <davorfilms@googlemail.com>
2021-07-18 18:29:09 +0200

    Add project files.

---
+++ YoutubeArchiveCDXProcessor/Capture.cs
@@ -0,0 +1,25 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeArchiveCDXProcessor
+{
+    public class Capture
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [Indexed]
+        public string Urlkey { get; set; }
+        public string Timestamp { get; set; }
+        public string Original { get; set; }
+        public string Mimetype { get; set; }
+        public int Statuscode { get; set; }
+        public string Digest { get; set; }
+        public int Length { get; set; }
+        public int Dupecount { get; set; }
+
+    }
+}
---
+++ YoutubeArchiveCDXProcessor/Program.cs
@@ -0,0 +1,106 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace YoutubeArchiveCDXProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ParseData();
+
+        }
+
+        static void ParseData()
+        {
+
+            string[] lines = File.ReadAllLines("cdx@url=youtube.com%2Fwatch%2A&output=json.json");
+
+
+
+
+            var db = new SQLiteConnection("data.db");
+            db.CreateTable<Capture>();
+
+            db.BeginTransaction();
+
+            // i=1 bc ignore first line
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                line = line.Trim("[], ".ToCharArray());
+                int lineLength = line.Length;
+
+                List<string> parts = new List<string>();
+                int partindex = 0;
+                bool isInsideQuote = false;
+                string currentChar = "";
+                string currentCharRaw = "";
+                string currentPart = "";
+                for(int k = 0; k < lineLength; k++)
+                {
+                    if(isInsideQuote && line[k].ToString() == "\\")
+                    {
+                        k++;
+                        currentChar = "\\"+line[k].ToString();
+                        currentCharRaw = line[k].ToString();
+                    } else
+                    {
+                        currentChar = currentCharRaw = line[k].ToString();
+                    }
+
+                    if(currentChar == "\"")
+                    {
+                        if (isInsideQuote)
+                        {
+                            parts.Add(currentPart);
+                            currentPart = "";
+                            isInsideQuote = false;
+                        }
+                        else
+                        {
+                            isInsideQuote = true;
+                        }
+                    }
+                    else if(isInsideQuote)
+                    {
+                        currentPart += currentCharRaw;
+                    }
+
+
+                }
+
+                int statuscode=0, length=0;
+                int.TryParse(parts[4], out statuscode);
+                int.TryParse(parts[6], out length);
+
+                Capture cap = new Capture() { 
+                    Urlkey = parts[0],
+                    Timestamp = parts[1],
+                    Original = parts[2],
+                    Mimetype = parts[3],
+                    Statuscode = statuscode,
+                    Digest = parts[5],
+                    Length = length//,
+                    //Dupecount = int.Parse(parts[7])
+                };
+
+                db.Insert(cap);
+
+                Console.WriteLine(String.Join("     ,    ",parts));
+                /*
+                if (i > 100)
+                {
+                    break;
+                }*/
+            }
+            db.Commit();
+            db.Close();
+            db.Dispose();
+            Console.ReadKey();
+        }
+    }
+}
